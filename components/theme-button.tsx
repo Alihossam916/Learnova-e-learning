56aeddc77f2296254881c777e3436cdbd589006c
Alihossam916 <alihossam916@gmail.com>
2026-03-18 19:35:51 +0200

    modified theme button style

--- components/theme-button.tsx
+++ components/theme-button.tsx
@@ -11,7 +11,7 @@ export function ModeToggle() {
     <Button
       variant="outline"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      className="fixed bottom-5 right-5 sm:bottom-10 sm:right-10 md:bottom-15 md:right-15 size-12 cursor-pointer border-2 hover:bg-accent hover:text-accent-foreground"
+      className="fixed z-40 bottom-5 right-5 sm:bottom-10 sm:right-10 md:bottom-15 md:right-15 size-12 cursor-pointer border-2 hover:bg-accent hover:text-accent-foreground"
     >
       <Sun className="size-[1.4rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
       <Moon className="absolute size-[1.4rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
