bb630ee7cd0eb0d4d106be06dbbf65281502b12b
Alihossam916 <alihossam916@gmail.com>
2026-03-30 20:47:27 +0200

    updated course card

--- components/coursesCard.tsx
+++ components/coursesCard.tsx
@@ -1,11 +1,7 @@
 "use client";
-import React from "react";
-import { useState } from "react";
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 import {
   Card,
-  CardAction,
   CardContent,
   CardDescription,
   CardFooter,
