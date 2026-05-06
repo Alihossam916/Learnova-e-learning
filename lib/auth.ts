7a7a5f2dbcb504c480cb0bdeca2a68b90989e0b2
Alihossam916 <alihossam916@gmail.com>
2026-05-06 15:01:58 +0300

    updated auth.ts to capitalize first and last names during sign-up process.

--- lib/auth.ts
+++ lib/auth.ts
@@ -29,8 +29,8 @@ export async function signUp(formData: FormData) {
 
   const newUser: User = {
     id: uuidv4(),
-    firstName,
-    lastName,
+    firstName: firstName.charAt(0).toUpperCase() + firstName.slice(1),
+    lastName: lastName.charAt(0).toUpperCase() + lastName.slice(1),
     email,
     password: hashedPassword,
     role,
