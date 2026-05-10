bcfde48d71238ddcfff68a8298cb2172f6fb1ec0
Alihossam916 <alihossam916@gmail.com>
2026-05-10 23:09:54 +0300

    created a placeholder page for the "Create Course" feature in the dashboard.

---
+++ app/(protected)/dashboard/create-course/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateCourse = () => {
+  return (
+    <div className="flex justify-center items-center px-12 pb-12 pt-8">
+      <p className="text-2xl font-bold">Coming Soon...</p>
+    </div>
+  );
+};
+
+export default CreateCourse;
